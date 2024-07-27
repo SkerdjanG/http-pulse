c8ed975242abb35d18268a83bda54816a23e43f9
Skerdi <skerdilol@gmail.com>
2024-07-27 14:42:35 +0200

    Fixed native build to load requests.pulse resource file in the native image and also fixed bug with jni provider.

---
+++ src/main/java/com/skerdy/httpulse/config/NativeResourcesHint.java
@@ -0,0 +1,21 @@
+package com.skerdy.httpulse.config;
+
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportRuntimeHints;
+
+@Configuration
+@ImportRuntimeHints(NativeResourcesHint.NativeResourcesRegistrar.class)
+public class NativeResourcesHint {
+
+    static class NativeResourcesRegistrar implements RuntimeHintsRegistrar {
+
+        @Override
+        public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+            hints.resources()
+                    .registerPattern("*.pulse");
+        }
+    }
+
+}
